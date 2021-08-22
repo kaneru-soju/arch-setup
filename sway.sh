9c46dc2fbbaea1ce37c36fae98bbf5bcce6c3120
kaneru-soju <sojusings@gmail.com>
2021-08-22 13:33:37 -0400

    sway/install-uefi changes

--- sway.sh
+++ sway.sh
@@ -1,11 +1,48 @@
 #!/bin/bash
 
-# use your aur helper of choice if you don't like paru
-git clone https://aur.archlinux.org/paru.git
-cd paru
+# use your aur helper of choice if you don't like yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
 makepkg -si
 
-sudo pacman -S sway swaylock swayidle kitty firefox-nightly wofi xorg-xwayland waybar mako catgirl mopidy-spotify ncmpcpp cava imv mpv ranger zathura htop neofetch unzip zip zsh zsh-autosuggestions zsh-syntax-highlighting
+# need to install Mononoki (NF), Material Design Icons Desktop, and Inter fonts, betterdiscord, and your browser of choice
+yay -S noto-color-emoji-fontconfig \
+sway \
+swayidle \
+kitty \
+wofi \
+xorg-xwayland \
+waybar \
+neofetch \
+sddm \
+slurp \
+grim \
+swappy \
+ranger \
+cava \
+imv \
+mpv \
+ranger \
+zathura \
+neofetch \
+unzip \
+zip \
+gruvbox-dark-gtk \
+gruvbox-dark-icons-gtk \
+hexchat \
+imagemagick \
+mako \
+nautilus \
+paleofetch \
+qalculate-gtk \
+sddm \
+spotify \
+spicetify-cli \
+swaylock-effects-git \
+zsh \
+zsh-autosuggestions \
+zsh-syntax-highlighting \
+zsh-theme-powerlevel10k-git
 
 echo "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
 sleep 5
